8722e858a15b2d5c4007d0fa3da6628dc488c11a
Adel Hefny <lionadel1234@gmail.com>
2024-10-28 16:00:00 +0300

    Ignore .idea folder

--- ControlUnit.cpp
+++ ControlUnit.cpp
@@ -1,3 +1,33 @@
-//
-// Created by ALRWAD LAPTOP on 10/27/2024.
-//
+#include "ControlUnit.h"
+#include <bits/stdc++.h>
+#include <bitset>
+
+using namespace std;
+
+// Definitions of CU methods
+
+void CU::load(int idxReg, int intMem, vector<Register>& reg, const Memory& mem) {
+    reg[idxReg].setData(mem.getData(intMem));
+}
+
+void CU::load(int idxReg, string val, vector<Register>& reg) {
+    reg[idxReg].setData(val);
+}
+
+void CU::store(string reg, string& mem) {
+    mem = reg;
+}
+
+void CU::mov(int reg1idx, int reg2idx, vector<Register>& reg) {
+    reg[reg1idx] = reg[reg2idx];
+}
+
+void CU::jump(int regIdx, int memIdx, vector<Register>& reg, string& pc, const Memory& memory) {
+    if (reg[regIdx].getData() != 0) {
+        pc = memory.getData(memIdx);
+    }
+}
+
+void CU::halt() {
+    haltFlag = true;
+}
--- ControlUnit.h
+++ ControlUnit.h
@@ -1,16 +1,21 @@
-//
-// Created by ALRWAD LAPTOP on 10/27/2024.
-//
-
 #ifndef VOLE_MACHINE_CONTROLUNIT_H
 #define VOLE_MACHINE_CONTROLUNIT_H
 
+class Register;
+class Memory;
 
+class CU {
+private:
+    bool haltFlag;
 
-class ControlUnit {
+public:
+    CU() : haltFlag(false) {}
 
+    void load(int idxReg, int intMem, vector<Register>& reg, const Memory& mem);
+    void load(int idxReg, string val, vector<Register>& reg);
+    void store(string reg, string& mem);
+    void mov(int reg1idx, int reg2idx, vector<Register>& reg);
+    void jump(int regIdx, int memIdx, vector<Register>& reg, string& pc, const Memory& memory);
+    void halt();
 };
-
-
-
-#endif //VOLE_MACHINE_CONTROLUNIT_H
+#endif
