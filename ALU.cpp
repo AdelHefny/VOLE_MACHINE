2210c14b2c8c7eb74257aeebe784a0abf9226a36
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2024-11-01 19:22:27 +0200

    Update ALU.cpp

--- ALU.cpp
+++ ALU.cpp
@@ -4,7 +4,12 @@
 
 using namespace std;
 
-float ALU::binToFloat(string bin) {
+ALU::ALU() : registers(16) {}
+
+float ALU::hexToFloat(string hex) {
+    int x = hexToDec(hex);
+    bitset<8> bs(x);
+    string bin = bs.to_string();
     int sign = bin[0] - '0', bias = 4;
     int exponent = bitset<3>(bin.substr(1, 3)).to_ulong();
     int mantissa = bitset<4>(bin.substr(4, 4)).to_ulong();
@@ -20,45 +25,71 @@ float ALU::binToFloat(string bin) {
     return res;
 }
 
-string ALU::decToBin(int dec) {
-    string bin = "";
-    while(dec > 0){
-        bin = to_string(dec%2) + bin;
-        dec /= 2;
-    }
-    while (bin.length() < 8) {
-        bin.insert(0, "0");
-    }
-    return (bin.size() > 0? bin : "00000000");
-}
-
 int ALU::hexToDec(string s) {
     return stoi(s, nullptr, 16);
 }
 
 string ALU::floatToBin(float f) {
     int sign = f < 0 ? 1 : 0;
+    if (f < -7.5) f = -7.5;
+    else if (f > 7.5) f = 7.5;
     f = fabs(f);
 
-    int exponent = 0;
-    while (f >= 1.0 && exponent < 4) {
-        f /= 2.0, exponent++;
-    }
-    while (f < 0.5 && exponent > -4) {
-        f *= 2.0, exponent--;
+    int intPart = static_cast<int>(f);
+    float frac = f - intPart;
+
+    string fracBin = "", intBin = "", Bin = "";
+    string mantissa = "";
+    while(frac > 0 && fracBin.size() < 8){
+        frac *= 2;
+        if(frac >= 1.0){
+            fracBin += "1";
+            frac -= 1.0;
+        }
+        else fracBin += "0";
     }
 
-    int biasedExponent = exponent + 4, mantissa = 0;
-    for (int i = 0; i < 4; ++i) {
-        f *= 2.0;
-        if (f >= 1.0) {
-            mantissa |= (1 << (3 - i));
-            f -= 1.0;
+    if (intPart == 0) intBin = "0";
+    else {
+        while (intPart > 0) {
+            intBin = (intPart % 2 == 0 ? "0" : "1") + intBin;
+            intPart /= 2;
         }
     }
 
-    string bin = to_string(sign) + bitset<3>(biasedExponent).to_string() + bitset<4>(mantissa).to_string();
-    return bin;
+    Bin = intBin + "." +fracBin;
+
+    int ex = 0;
+    auto it = find(Bin.begin(), Bin.end(), '.');
+    if (f > 1.0) {
+        ex = it - Bin.begin();
+        mantissa += Bin.substr(0, ex);
+        mantissa += Bin.substr(ex + 1, 4 - ex);
+    }
+    else {
+        int val = it - Bin.begin();
+        int indx = 0;
+        for (int i = val + 1; i < Bin.size(); ++i) {
+            if (Bin[i] == '1') {
+                indx = i;
+                break;
+            }
+            --ex;
+            if (ex == -4) {
+                indx = i + 1;
+                break;
+            }
+
+        }
+        mantissa = Bin.substr(indx, 4);
+    }
+
+    while(mantissa.size() < 4){
+        mantissa.insert(0,"0");
+    }
+
+    int biasedexp = ex + 4;
+    return (to_string(sign)  + bitset<3>(biasedexp).to_string() + mantissa);
 }
 
 string ALU::decToHex(int dec) {
@@ -67,23 +98,36 @@ string ALU::decToHex(int dec) {
     return ss.str();
 }
 
-string ALU::binToHex(string s) {
-    int x = stoi(s, nullptr, 2);
-    string hex = decToHex(x);
+string ALU::binToHex(string bin) {
+    unsigned long binValue = bitset<8>(bin).to_ulong();
+    stringstream ss;
+    ss << hex << binValue;
+    string hexValue = ss.str();
+    for (char& c : hexValue) {
+        c = toupper(c);
+    }
+    return hexValue;
+}
+
+string ALU::add(int r1, int r2, int r3) {
+    float f1 = hexToFloat(registers[r1]);
+    float f2 = hexToFloat(registers[r2]);
+    float ans = f1 + f2;
+    string bin = floatToBin(ans);
+    string hex = binToHex(bin);
     return hex;
 }
 
-void ALU::add(int r1, int r2, int r3,Register& registers) {
-    string bin1 = decToBin(registers.get_value(r1));
-    string bin2 = decToBin(registers.get_value(r2));
+string ALU::twosComp(int r1, int r2, int r3) {
+    int a = hexToDec(registers[r1]), b = hexToDec(registers[r2]);
+    if(a >= 0x80)a -= 256;
+    if(b >= 0x80)b -= 256;
 
-    float f1 = binToFloat(bin1), f2 = binToFloat(bin2);
+    int ans = a + b;
 
-    float ans = f1 + f2;
+    if(ans > 127)ans -= 256;
+    else if(ans < -128)ans += 256;
 
-    if (ans < -7.5) ans = -7.5;
-    else if (ans > 7.5) ans = 7.5;
-
-    string bin = floatToBin(ans);
-    string hex = binToHex(bin);
+    string res = decToHex(ans);
+    return res;
 }
